a71ec84e05dea80b501965f47e9f6fc36b4b84f5
Hamanor <rdomanoel@gmail.com>
2018-08-08 23:30:52 -0300

    developing new tests for actions creator, add some middlewares in the root component

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,19 @@
-import { SAVE_COMMENT } from 'actions/types';
+import axios from 'axios';
+import { SAVE_COMMENT, FETCH_COMMENTS } from 'actions/types';
 
 export function saveComment(comment) {
     return {
         type: SAVE_COMMENT,
         payload: comment
     };
+}
+
+export function fetchComments() {
+    const URL = 'http://jsonplaceholder.typicode.com/comments';
+    const response = axios.get(URL)
+
+    return {
+        type: FETCH_COMMENTS,
+        payload: response
+    }
 }
