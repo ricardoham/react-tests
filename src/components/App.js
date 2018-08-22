db92096f096b3c95bcb06125512e0e3410b106fa
Hamanor <rdomanoel@gmail.com>
2018-08-21 22:29:31 -0300

    some new files to HOC

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import CommentBox from './comment-box';
 import CommentList from './comment-list';
+import * as actions from 'actions';
 
-export default () => {
+class App extends Component {
+
+    renderButton () {
+        if (this.props.auth) {
+            return (
+                <button onClick={() => this.props.changeAuth(false)}>
+                    Sing Out
+                </button>
+            )
+        } else {
+            return (
+                <button onClick={() => this.props.changeAuth(true)}>
+                    Sing In
+                </button>
+            )
+        }
+    }
+
+    renderHeader() {
+        return (
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/post">Post a Comment</Link></li>
+                <li>
+                    {this.renderButton()}
+                </li>
+            </ul>
+        );
+    };
+
+    render() {
     return (
         <div>
-            <CommentBox />
-            <CommentList />
+            {this.renderHeader()}
+            <Route path="/post" component={CommentBox} />
+            <Route path="/" exact component={CommentList} />
         </div>
-    );
+        );
+    }
 };
+
+function mapStateToProps(state) {
+    return {
+        auth: state.auth,
+    }
+}
+
+export default connect(mapStateToProps, actions)(App);
--- src/components/comment-box.js
+++ src/components/comment-box.js
@@ -8,6 +8,23 @@ class CommentBox extends Component {
         comment: ''
     };
 
+    componentDidMount() {
+        this.shouldNavigateAway();
+
+    }
+
+    // o component ira re-render toda a vez que ele receber
+    // um novo props
+    componentDidUpdate() {
+        this.shouldNavigateAway();
+    }
+
+    shouldNavigateAway() {
+        if (!this.props.auth) {
+            this.props.history.push('/');
+        }
+    }
+    
     handleChange = (event) => {
         this.setState({
             comment:event.target.value
@@ -38,4 +55,10 @@ class CommentBox extends Component {
     }
 };
 
-export default connect(null, actions)(CommentBox);
+function mapStateToProps(state) {
+    return {
+        auth: state.auth,
+    };
+};
+
+export default connect(mapStateToProps, actions)(CommentBox);
